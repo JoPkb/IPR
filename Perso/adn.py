c541f84972d57acacd2180a9b7776632bd27b694
JoPkb <56796390+JoPkb@users.noreply.github.com>
2020-04-20 13:03:37 +0200

    Update adn.py

--- Perso/adn.py
+++ Perso/adn.py
@@ -1,40 +1,49 @@
-
+import time
 def composition(sequence_adn) :
-    composition = {'A' : 0, 'T' : 0, 'C' : 0, 'G' : 0}
+    a = time.time()
+    composition = {'a' : 0, 't' : 0, 'c' : 0, 'g' : 0}
 
-    for nucleotide in sequence_adn :
-        composition[nucleotide] += 1
+    for lettre in composition :
+        composition[lettre] = sequence_adn.count(lettre)
 
-    return composition
+    b = time.time()
+
+    return b-a
 
 
 def pourcentGC(composition) :
-    total = 0
-    liste_valeurs = [a for a in composition.values()]
 
-    for valeur in liste_valeurs :
-        total += valeur
-
-    pourcent = ((composition['G'] + composition['C']) / total) * 100
+    pourcent = ((composition['g'] + composition['c']) / sum(composition.values())) * 100
 
     return pourcent
 
 
 def temp_fusion_howley(composition) :
-    total = 0
-    liste_valeurs = [a for a in composition.values()]
-    pourcent = pourcentGC(composition)
 
-    for valeur in liste_valeurs :
-        total += valeur
+    total = sum(composition.values())
+    pourcent = pourcentGC(composition)
 
     return 67.5 + (0.34*pourcent)-(395/total)
 
 
+def est_complementaire(seq1, seq2):
+    seq2 = list(seq2)
+    seq2.reverse()
+    seq2 = ''.join(seq2)
+
+    if seq1 == seq2 :
+        return True
+    else :
+        return False
 
 
 
 
 
 if __name__ == '__main__' :
-    print(temp_fusion_howley(composition('AAAACCCGGGGTTTT')))
+    print(composition('atgagtgaacgtctgagcattaccccgctggggccgtatatcggcgca'))
+    # seq = 'atgagtgaacgtctgagcattaccccgctggggccgtatatcggcgca'
+    # print(pourcentGC(composition(seq)))
+    # print(temp_fusion_howley(composition(seq)))
+    #
+    # print(est_complementaire('acg', 'aca'))
