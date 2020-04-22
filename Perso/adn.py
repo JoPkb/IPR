561680111b80b05078c494a8948c0868b4d4b9ee
JoPkb <56796390+JoPkb@users.noreply.github.com>
2020-04-22 03:41:19 +0200

    adding ADN2ARN, traduction, and create_sequence

--- Perso/adn.py
+++ Perso/adn.py
@@ -1,4 +1,15 @@
 import time
+import random
+def create_seq(length):
+    nucleotides = ['a','t','g','c']
+    seq = ''
+    for i in range(length):
+        chooser = random.randint(0,3)
+        seq += nucleotides[chooser]
+
+
+    return seq
+
 def composition(sequence_adn) :
     a = time.time()
     composition = {'a' : 0, 't' : 0, 'c' : 0, 'g' : 0}
@@ -37,11 +48,35 @@ def est_complementaire(seq1, seq2):
         return False
 
 
+def ADN2ARN(seq):
+    return seq.replace('t', 'u')
+
+def traduction(seq) :
+
+    code_genetique = {'uuu': 'F', 'ucu': 'S', 'uau': 'Y', 'ugu': 'C','uuc': 'F', 'ucc': 'S', 'uac': 'Y', 'ugc': 'C','uua': 'L', 'uca': 'S', 'uaa': '*', 'uga': '*','uug': 'L', 'ucg': 'S', 'uag': '*', 'ugg': 'W','cuu': 'L', 'ccu': 'P', 'cau': 'H', 'cgu': 'R','cuc': 'L', 'ccc': 'P', 'cac': 'H', 'cgc': 'R','cua': 'L', 'cca': 'P', 'caa': 'Q', 'cga': 'R','cug': 'L', 'ccg': 'P', 'cag': 'Q', 'cgg': 'R','auu': 'I', 'acu': 'T', 'aau': 'N', 'agu': 'S','auc': 'I', 'acc': 'T', 'aac': 'N', 'agc': 'S','aua': 'I', 'aca': 'T', 'aaa': 'K', 'aga': 'R','aug': 'M', 'acg': 'T', 'aag': 'K', 'agg': 'R','guu': 'V', 'gcu': 'A', 'gau': 'D', 'ggu': 'G','guc': 'V', 'gcc': 'A', 'gac': 'D', 'ggc': 'G','gua': 'V', 'gca': 'A', 'gaa': 'E', 'gga': 'G','gug': 'V', 'gcg': 'A', 'gag': 'E', 'ggg': 'G',}
+    aa = ''
+    cadre = int(input('Entrez le cadre de lecture :0, 1 ou 2 :  '))
+
+    if cadre != 0 and cadre != 1 and cadre != 2 :
+        raise ValueError
+
+    seq = ADN2ARN(seq)
+    for i in range(cadre, len(seq)-len(seq)%3, 3) :
+        aa += code_genetique[seq[i:i+3]]
+
+
+    return (aa, len(aa))
+
 
 
 
 if __name__ == '__main__' :
-    print(composition('atgagtgaacgtctgagcattaccccgctggggccgtatatcggcgca'))
+    a = time.time()
+    seq = create_seq(3000000)
+    trad = traduction(seq)
+    print(trad[0],'\nlen :\n', trad[1])
+    b = time.time()
+    print(b-a)
     # seq = 'atgagtgaacgtctgagcattaccccgctggggccgtatatcggcgca'
     # print(pourcentGC(composition(seq)))
     # print(temp_fusion_howley(composition(seq)))
